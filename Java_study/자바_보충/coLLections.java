a7e1b6e130704e6a4ca973c510b0185f1dd0da6d
KSH <tkdgns543@gmail.com>
2022-10-21 23:23:43 +0900

    127부터

--- Java_study/자바_보충/coLLections.java
+++ Java_study/자바_보충/coLLections.java
@@ -1,5 +1,6 @@
 package Java_study.자바_보충;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class coLLections {
@@ -30,6 +31,23 @@ public class coLLections {
 //        반복문으로 ArrayList 삭제할때는 뒤 인덱스부터 제거
 
 
+        List lList=new LinkedList();
+//        일반 배열은 접근시간이 짧은 장점
+//        크기를 변경할 수 없어 새로운 배열을 생성후 복사해야함. 크기변경을 피하기 위해 여유로운 크기의 배열을 생성할 경우 메모리낭비
+//        비순차적인 데이터의 추가, 삭제에 시간 많이 걸림.그러나 끝부분의 추가와 삭제는 빠르다
+//        LinkedList는 불연속적으로 존재하는 데이터를 연결 유연한 추가,삭제, 대신 접근성이 나쁨
+
+        Stack stk=new Stack();
+
+//        큐는 인터페이스이다
+        Queue que = new LinkedList();
+
+
+//        Iterator, Enumeration 은 사실상 같다. 컬렉션에 저장된 데이터를 접근하는데 필요한 인터페이스
+//        컬렉션들의 여로 종류에 저장된 요소들을 읽어오는 방법을 표준화한것
+//        ListIterator는 next 뿐만아니라 previous도 있다
+//        Iterator는 일회용이기 때문에 처음부터 다시 읽으려면 새로 만들어야함
+
         Set set=new HashSet();
         set.addAll(list); //합집합
         set.containsAll(list); //부분집합
@@ -38,6 +56,7 @@ public class coLLections {
 
 
 
+//        Map에는 Iterator가 없다. Collection의 자손이 아님
         Map map=new HashMap();
         map.clear();
         map.put(3,4);
@@ -47,8 +66,24 @@ public class coLLections {
         Set ets=map.entrySet(); //key-value 쌍
         Set key=map.keySet(); //key만
         map.values(); //value만
+
+//        Map에서 Iterator를 쓰는방법
+        Iterator eit=ets.iterator();
         System.out.println(ets);
         System.out.println(key);
         System.out.println(map.values());
+
+
+
+
+        List lst= Arrays.asList(new Integer[]{1,2,3,4,5});
+//        위에와 같음
+//        List lst=Arrays.asList(1,2,3,4,5);
+
+//        위 둘은 읽기전용이라 lst.add(6) 이런거 안됨
+//        할려면
+//        List list=new ArrayList(Arrays.asList(1,2,3,4,5));
+//        이런 식으로
+
     }
 }
