2d25127362259b0033e9e516e5ed201880ee81a0
KSH <tkdgns543@gmail.com>
2022-10-26 12:13:08 +0900

    139부터

--- Java_study/자바_보충/geNerics.java
+++ Java_study/자바_보충/geNerics.java
@@ -1,6 +1,8 @@
 package Java_study.자바_보충;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class geNerics {
     public static void main(String[] args) {
@@ -16,5 +18,35 @@ public class geNerics {
         Integer i=arrayList.get(0);
 
 //        원래대로 여러 종류의 객체를 넣고 싶으면 안에 Object를 넣으면 됨
+
+//        다음과 같이 참조변수와 생성자의 대입된 타입은 일치해야함
+        List<Product> tvlist=new ArrayList<Product>();
+
+//        오류
+//        List<Product> tvList2=new ArrayList<Tv>();
+
+//        지네릭 클래스간의 다형성은 성립(여전히 대입된 타입은 일치해야)
+        List<Tv> tvList2=new ArrayList<Tv>();
+        List<Tv> tvList3=new LinkedList<Tv>();
+
+//        매개변수의 다형성 성립
+        tvlist.add(new Product());
+        tvlist.add(new Tv());
+
+//        원 타입이 Product이기 때문에 첫번째는 형변환 불필요. 두번째는 Tv로 형변환 필요.
+        Product product=tvlist.get(0);
+        Tv tv=(Tv)tvlist.get(1);
+
+        List<>
+
     }
 }
+
+class Product{}
+class Tv extends Product{}
+
+
+class Fruit{}
+
+//Fruit의 자손만 받을 수 있음
+class FruitBox<T extends Fruit>{}
