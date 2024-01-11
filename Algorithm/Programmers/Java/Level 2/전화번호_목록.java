fb8eb51eb23c8e3a32d8660a8124e865c3525579
kgpksh <tkdgns543@gmail.com>
2024-01-11 17:10:58 +0900

    fix: 소스파일 명과 클래스명 불일치 제거

--- Algorithm/Programmers/Java/Level 2/전화번호_목록.java
+++ Algorithm/Programmers/Java/Level 2/전화번호_목록.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-class Solution {
+class 전화번호_목록 {
     public boolean 전화번호_목록(String[] phone_book) {       
         int L = phone_book.length;
         Arrays.sort(phone_book);
